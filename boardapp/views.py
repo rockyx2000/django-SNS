5393296fb923df3d35cfaf1e5f8c140f59215740
Hiroki Ohtake <hametu1992@gmail.com>
2022-08-31 21:49:13 +0900

    add auth function

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from sqlite3 import IntegrityError
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.db import IntegrityError
 
 # Create your views here.
+
+def signupfunc(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        try:
+            user = User.objects.create_user(username, "", password)
+            return redirect("login")
+        except IntegrityError:
+            return render(request, 'signup.html', {"error": "すでに作成されているユーザーです"})
+    return render(request, 'signup.html', {"some": '100'})
+
+def loginfunc(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'login.html', {"context": "ログインしました"})
+        else:
+            return render(request, 'login.html', {"context": "ログインできませんでした"})
+    return render(request, 'login.html', {})
